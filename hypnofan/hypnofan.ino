6a6c4dec7fa54322386f647a73bf5e70acf75316
Mathsterk <mathiassterk@gmail.com>
2016-05-30 11:07:12 +0200

    malinux edition

--- hypnofan/hypnofan.ino
+++ hypnofan/hypnofan.ino
@@ -1,79 +1,74 @@
-
+// setter noen globale variabler
 unsigned long ticktime = 0;
 unsigned long currentMicros = 0;
 unsigned long shift = 0;
 
-
-int fancolor = 0;
-
+int fanColor = 0;
 int fanCount = 0;
 
-
+// denne kjøres bare 1 gang
 void setup() {
 
+// setter opp hva pinsene brukes til, om de skal drive noe eller bare leses ifra
   pinMode(2, INPUT);
   pinMode(13, OUTPUT);
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(2), fantrip, FALLING);
 
+  // setter opp en interrupt (når noe skjer, uansett hva arduinoen holder på med, kjør denne funksjonen)
+  attachInterrupt(digitalPinToInterrupt(2), fantrip, FALLING);
+// ut av løkka
 }
 
+// denne loopes til arduinoen resettes, tryner eller slåes av
 void loop() {
 
 // if(!digitalRead(2)) {
-//   // if (fancolor == 0) {
+//   // if (fanColor == 0) {
 //    fanCount++;
 //   }
 
-  // if (fanCount >= 2 && fancolor == 0) {
+  // if (fanCount >= 2 && fanColor == 0) {
+
+    // if-løkke, om fanCount er større eller lik 2, kjører den
   if (fanCount >= 2) {
-    // if (fancolor == 0) {
+    // if (fanColor == 0) {
+      // setter variabelen ticktime til hva funksjonen micros returnerer
     ticktime = micros();
+    // setter variabelen fanCount til 0
     fanCount = 0;
+    // ut av if-løkka
   }
-  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
+
+  // setter currentMicros til hva funksjonen micros returnerer
   currentMicros = micros();
 
-  // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-  // if (abs(ticktime-millis()) <= 10000 || ticks) {
-  // if (!triggered) {
-  //   ticktime = millis();
-  // }
-
-  // if (currentMicros - ticktime <= 5) {
-  //   pixels.setPixelColor(0, pixels.Color(255, 255, 255)); // Moderately bright green color.
-  //   triggered = true;
-  // } else {
-  //   pixels.setPixelColor(0, pixels.Color(0, 0, 0));
-  // }
-  // if (currentMicros - ticktime >= 9) {
-  //       triggered = false;
-  // }
-
-
+ // om currentMicros minus ticktime er større eller lik 2100 så kjøres den
   if (currentMicros - ticktime >= 2100) {
-    fancolor = 0;
+    fanColor = 0;
     // ticktime = micros();
   }
 
+ // om currentMicros minus ticktime er mindre eller lik 6000 ELLER currentMicros minus ticktime er større eller lik 2000
   if (currentMicros - ticktime <= 6000 && currentMicros - ticktime >= 2000) {
-    fancolor = 1;
+    fanColor = 1;
   }
     if (currentMicros - ticktime <= 2000 && currentMicros - ticktime >= 1500) {
-    fancolor = 4;
+    fanColor = 4;
   }
     if (currentMicros - ticktime <= 1500 && currentMicros - ticktime >= 500) {
-    fancolor = 3;
+    fanColor = 3;
   }
     if (currentMicros - ticktime <= 500 && currentMicros - ticktime >= 0) {
-    fancolor = 4;
+    fanColor = 4;
   }
 
-
-  switch (fancolor) {
+// denne er litt interessant, det er som mange if-løkker, om fanColor er 1, 2 ,3 etc
+  switch (fanColor) {
+    // om fanColor er 0, så kjøres funksjonen showled
   case 0: showled(0,  0,  0);
+  // ut av case 0
     break;
   case 1: showled(1, 0, 0);
     break;
@@ -87,12 +82,18 @@ void loop() {
 
 }
 
+// dette er en funksjon, den returnerer ingenting (void), den heter showled, og tar imot 3 parametere, red, green og blue, de må være en integer (helt tall)
 void showled(int red, int green, int blue) {
+  // skriv til pin 9, 1 (true) minus variabelen rød
   digitalWrite(9, 1 - red);
+
   digitalWrite(10, 1 - green);
   digitalWrite(11, 1 - blue);
+  // ut av funksjonen
 }
 
+// enda en funksjon, returnerer ingenting, og heter fantrip
 void fantrip() {
+  // øk verdien til fanCount med 1
   fanCount++;
 }
